1acad863b8836f2f2f980d7769aa3a4daf3e39a5
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-12-24 08:43:25 +0100

    Create TwoDiscsToCoupleSpheresSamplingDatasNonHolomorphButAbelianGroups.h

---
+++ TwoDiscsToCoupleSpheresSamplingDatasNonHolomorphButAbelianGroups.h
@@ -0,0 +1,11 @@
+//if we take two topological equations and refactore them in a model, we still cannot omit latent variables. Then,, here we use two full spheres
+//to sample communication datas that could be agebraic truth and, then, we can omit holomorph and cryptographic sampling functions.
+//instead, we better rely on 10d datas functions.
+
+
+//the reverting process is not really a process if the hamiltonians of sended models
+//could parse refactored latent variables on models. So, the reverse process needs 2000 iterations (mass transfer engeniring)
+class  SampleCoupleSphereReverseTwoDiscAlgorithm
+{
+
+}
